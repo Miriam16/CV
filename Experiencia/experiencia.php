4fc8099c21918b3c462e2cc8b97c455a5e3b5e86
Miriam Calo <meccalo@hotmail.com>
2023-02-07 11:46:26 -0300

    cuarto commit

--- Experiencia/experiencia.php
+++ Experiencia/experiencia.php
@@ -3,7 +3,7 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Educación</title>
-      <link rel="stylesheet" href="/Experiencia/experiencia.css">
+      <link rel="stylesheet" href="Experiencia/experiencia.css">
       <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Sevillana&display=swap" rel="stylesheet">
       <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
