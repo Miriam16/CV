e9922c37b5e92b0237743fd600cafcd859a4a153
Miriam Calo <meccalo@hotmail.com>
2023-02-07 11:36:21 -0300

    Segundo commit

--- DatosPersonales/datos.php
+++ DatosPersonales/datos.php
@@ -3,7 +3,7 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Datos Personales</title>
-      <link rel="stylesheet" href="/ProyectoFinalIntegrador/DatosPersonales/datos.css">
+      <link rel="stylesheet" href="/DatosPersonales/datos.css">
       <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Sevillana&display=swap" rel="stylesheet">  
       <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
